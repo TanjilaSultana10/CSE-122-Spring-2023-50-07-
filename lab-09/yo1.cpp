817e07f8e44011ff43330e3a804e0c0e5c8bacf8
Tanjila Sultana <111008105+TanjilaSultana10@users.noreply.github.com>
2023-03-14 11:21:35 +0600

    Add files via upload

---
+++ lab-09/yo1.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int k,w,n,sum=0;
+    cin>> k>>n >>w;
+    for(int i=1;i<=w;i++)
+    {
+        sum=sum+i;
+
+    }
+    int d=(sum*k)-n;
+    if (d<0)
+        cout<<"0";
+    else
+        cout<<d;
+}
