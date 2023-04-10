a142daa79fdb5999897ae51a7ffb14204643341c
Tanjila Sultana <111008105+TanjilaSultana10@users.noreply.github.com>
2023-04-10 21:30:31 +0600

    Update c02.cpp

--- Assignment-2/c02.cpp
+++ Assignment-2/c02.cpp
@@ -1,3 +1,13 @@
+/*Imagine a publishing company that markets both book and audiocassette
+versions of its works. Create a class publication that stores the title (a
+string) and price (type float) of a publication. From this class derive two
+classes: book, which adds a page count (type int), and tape, which adds a
+playing time in minutes (type float). Each of these three classes should
+have a getData() function to get its data from the user at the keyboard, and
+a putData() function to display its data.
+Write a main program to test the book and tape classes by creating
+instances of them, asking the user to fill in data with getData(), and then
+displaying the data with putData().*/
 #include<iostream>
 #include<string>
 using namespace std;
@@ -5,7 +15,6 @@ using namespace std;
 class publication
 {
     string title;
-
     float price;
 
     public:
@@ -14,23 +23,18 @@ void getdata()
 
 {
     cout<<"Enter title: ";
-
     cin>>title;
-
     cout<<"Enter price: ";
-
     cin>>price;
 }
 void putdata()
 {
     cout<<"Title: "<<title<<endl;
-
     cout<<"Price: "<<price<<endl;
 }
 };
 class book:public publication
 {
-
     int page_count;
 
     public:
@@ -38,21 +42,17 @@ class book:public publication
 void getdata()
 {
     publication::getdata();
-
     cout<<"Enter page count: ";
-
     cin>>page_count;
 }
 void putdata()
 {
     publication::putdata();
-
     cout<<"Page count: "<<page_count<<endl;
 }
 };
 class tape:public publication
 {
-
     float play_time;
 
     public:
@@ -60,16 +60,13 @@ class tape:public publication
 void getdata()
 {
     publication::getdata();
-
     cout<<"Enter Play time: ";
-
     cin>>play_time;
 }
 void putdata()
 
 {
     publication::putdata();
-
     cout<<"Play time: "<<play_time<<endl;
 }
 };
@@ -77,19 +74,12 @@ void putdata()
 int main()
 {
     book b;
-
     tape ta;
-
     cout<<"Enter Details Of Book:\n";
-
     b.getdata();
-
     cout<<"\nEnter details of Tape:\n";
-
     ta.getdata();
-
     b.putdata();
-
     ta.putdata();
 
     return 0;
